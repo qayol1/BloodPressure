dd0824cd6089b72f2f7a023a3a825529435ce580
qayol1 <qayol@freemail.hu>
2017-09-14 10:42:27 +0200

    Salted password salting implemented

---
+++ BloodPressure/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BloodPressure
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.UserTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.RePasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(75, 66);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 0;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(104, 102);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(47, 23);
+            this.LoginButton.TabIndex = 8;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // UserTextBox
+            // 
+            this.UserTextBox.Location = new System.Drawing.Point(75, 34);
+            this.UserTextBox.Name = "UserTextBox";
+            this.UserTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UserTextBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "User:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Password:";
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(250, 102);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(75, 23);
+            this.RegisterButton.TabIndex = 12;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(210, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Re:";
+            // 
+            // RePasswordTextBox
+            // 
+            this.RePasswordTextBox.Location = new System.Drawing.Point(240, 66);
+            this.RePasswordTextBox.Name = "RePasswordTextBox";
+            this.RePasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RePasswordTextBox.TabIndex = 14;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 150);
+            this.Controls.Add(this.RePasswordTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.UserTextBox);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Name = "Main";
+            this.Text = "Secret Application Name";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox UserTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox RePasswordTextBox;
+    }
+}
+
---
+++ BloodPressure/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BloodPressure
+{
+    public partial class Main : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=CODECOOL\\PETER;Initial Catalog=Blood;Integrated Security=True");
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private Boolean Verify(string savedPasswordHash, string password)
+        {
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+            return true;
+        }
+
+        private string Hash(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var pbkdf2 = new Rfc2898DeriveBytes(PasswordTextBox.Text, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            return savedPasswordHash;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+            string sql = "SELECT password FROM[Blood].[dbo].[User] u " +
+                "INNER JOIN [Blood].[dbo].[Password] p on u.PasswordId = p.Id WHERE u.LoginName = @LoginName";
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@LoginName", UserTextBox.Text);
+                SqlDataReader dataReader = command.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        if (Verify(dataReader.GetString(0), PasswordTextBox.Text))
+                        {
+                            MessageBox.Show("Succesfully logged in!");
+                        } else
+                        {
+                            MessageBox.Show("Invalid user name or password!");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid user name or password!");
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }        
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (PasswordTextBox.Text.Equals(RePasswordTextBox.Text) && !PasswordTextBox.Text.Equals("") && !UserTextBox.Text.Equals(""))
+            {  
+                int PasswordId;
+                bool IsNewUser = CheckUserName();
+                string HashedPassword = Hash(PasswordTextBox.Text);
+
+                if (IsNewUser) {
+                    PasswordId = GetPasswordId();
+                    if ( SavePassword(PasswordId,HashedPassword) && SaveNewUser(PasswordId))
+                    {
+                        MessageBox.Show("User: " + UserTextBox.Text + " succesfully created!");
+                        UserTextBox.Clear();
+                        PasswordTextBox.Clear();
+                        RePasswordTextBox.Clear();
+                    }
+                } 
+            }
+            else MessageBox.Show("The passwords/user are differents or empty!");
+        }
+
+        private bool SaveNewUser(int PasswordId)
+        {
+            int? UserId = null;
+            bool result = false;
+
+            try
+            {
+                connection.Open();
+                string sql = "Select Max(Id) from [Blood].[dbo].[User]";
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader dataReader = command.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        UserId = dataReader.GetInt32(0);
+                    }  
+                } else
+                {
+                    MessageBox.Show("User table is empty");
+                }
+                
+                dataReader.Close();
+                
+                sql = "Insert into [Blood].[dbo].[User] (Id,LoginName,PasswordId) Values (@Id,@LoginName,@PasswordId)";
+                try
+                {
+                    command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@Id", UserId + 1);
+                    command.Parameters.AddWithValue("@LoginName", UserTextBox.Text);
+                    command.Parameters.AddWithValue("@PasswordId", PasswordId + 1);
+                    command.ExecuteNonQuery();
+                    result = true;
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("{0} Exception caught.", ex);
+                }
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+            return result;
+        }
+
+        private bool CheckUserName()
+        {
+            bool result = false;
+            string sql = "Select LoginName from [Blood].[dbo].[User] where LoginName = @LoginName";
+            
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@LoginName", UserTextBox.Text);
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    MessageBox.Show("The user name is already exists!");
+                    result = false;
+                } else
+                {
+                    result = true;
+                }
+
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+            return result;
+        }
+
+        private int GetPasswordId()
+        {
+            int PasswordId = 0;
+            string sql = "Select Max(Id) from [Blood].[dbo].[Password]";
+            
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader dataReader = command.ExecuteReader();
+                try
+                {
+                    if (dataReader.HasRows)
+                    {
+                        while (dataReader.Read())
+                        {
+                            PasswordId = dataReader.GetInt32(0);
+                        }
+                     }
+                    else
+                    {
+                        MessageBox.Show("User table is empty");
+                    }
+                } catch
+                {
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+            return PasswordId;
+        }
+
+        private bool SavePassword(int PasswordId, string HashedPassword)
+        {
+            bool result = false;
+            string sql = "Insert into [Blood].[dbo].[Password] (Id,password) Values (@Id,@HashedPassword)";
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@HashedPassword", HashedPassword);
+                command.Parameters.AddWithValue("@Id", PasswordId+1);
+                command.ExecuteNonQuery();
+                result = true;
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+            return result;
+        }
+    }
+}
